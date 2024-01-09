878f1caed5159f1796de5318510afe3400d8f670
ib407ov <44349161+ib407ov@users.noreply.github.com>
2024-01-09 21:30:13 +0200

    Update 9 Janur 24

--- HW/week_1/6/6.go
+++ HW/week_1/6/6.go
@@ -2,30 +2,48 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
+
+//Функцию FilterAndSortMap, которая принимает карту (map)
+//с ключами и значениями типа string и две функции: фильтр
+//(функция, которая принимает string и возвращает bool)
+//и компаратор (функция, которая сравнивает две строки и
+//возвращает bool). Функция должна возвращать отсортированный
+//срез ключей, значения которых проходят через фильтр.
+
+//Привет, по 6 заданию тебе нужно дописать
+//функцию FilterAndSortMap и main перечитай
+//еще раз задание, что у тебя подается на вход
+//и что тебе надо получить на выходе. У тебя есть
+//пример в виде теста.
 
 func FilterAndSortMap(m map[string]string, filterFunc func(string) bool, compareFunc func(string, string) bool) []string {
 	var keys []string
 
-	for k := range m {
-		if filterFunc(k) {
+	for k, val := range m {
+		if filterFunc(val) {
 			keys = append(keys, k)
 		}
 	}
 
-	//sort.Slice(keys, func(i, j int) bool {
-	//	return compareFunc(keys[i], keys[j])
-	//})
+	sort.Slice(keys, func(i, j int) bool {
+		return compareFunc(keys[i], keys[j])
+	})
 
 	return keys
 }
 
 func filterFunc(s string) bool {
-	return true
+	if len(s) > 6 {
+		return true
+	}
+
+	return false
 }
 
 func compareFunc(a, b string) bool {
-	if a == b {
+	if a < b {
 		return true
 	}
 	return false
@@ -35,8 +53,9 @@ func main() {
 	m := map[string]string{
 		"a": "apple",
 		"b": "banana",
-		"z": "cherry",
+		"c": "cherry",
 	}
 
+	fmt.Println()
 	fmt.Println(FilterAndSortMap(m, filterFunc, compareFunc))
 }
